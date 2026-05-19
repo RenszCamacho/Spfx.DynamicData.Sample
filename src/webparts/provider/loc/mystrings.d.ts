42a3d5cb24b11e0d31d8ee0221e8a9da431fda3d
Renszo Camacho <varsovia84@gmail.com>
2026-05-19 14:25:50 +0200

    chore: scaffold SPFx project
    - This is the starter scaffold by yoman generator

---
+++ src/webparts/provider/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IProviderWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ProviderWebPartStrings' {
+  const strings: IProviderWebPartStrings;
+  export = strings;
+}
